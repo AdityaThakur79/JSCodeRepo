b69033f42f6184edbc7cd3cb378a3372ab1f3685
Thakur Aditya <121376593+AdityaThakur79@users.noreply.github.com>
2023-10-06 09:06:04 +0000

    Datatypes in JS

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) we are using node js and not browser
+
+console.log(3+3)
+console.log("Aditya")
+
+let name ="Aditya"
+let age = 20
+let isLoggedIn = true
+let state;
+
+console.table([typeof(name),typeof(age),typeof(isLoggedIn),typeof(state),typeof(null)]);
+
+//Number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+// ***Null is an object***
+
+//object 
+ 
+
+
