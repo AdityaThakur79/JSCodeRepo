278c8650b3a99248ed6ad2d3f281bc32e82ed859
Thakur Aditya <121376593+AdityaThakur79@users.noreply.github.com>
2023-10-07 10:26:23 +0000

    Numbers and Maths

---
+++ 01_basics/07_numsAndMaths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score)
+// console.log(typeof score)
+
+const balance = new Number(200000)
+// console.log(balance)
+// console.log(typeof balance)
+
+//Converting number into string give access to all the methods of string datatype
+// console.log(balance.toString());
+// console.log(typeof balance)
+
+//toFixed used in ecommerce website
+// console.log(balance.toFixed(2))
+
+const anotherNum = 23.158
+//Gives Precise Value 
+// console.log(anotherNum.toPrecision(3))
+
+// console.log(balance.toLocaleString('en-IN'))
+
+//*************Maths***************
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.49999))
+// console.log(Math.ceil(4.0000000001))
+// console.log(Math.floor(4.999999))
+
+// console.log(Math.min(1,5,8,6))
+// console.log(Math.max(1,5,8,6))
+
+// console.log(Math.random());
+// const min = 10
+// const max = 30
+
+// console.log(Math.floor(Math.random() * (max-min)+1) + min) 
